ae9f502870cfe1e96b8da1e2014ce94971347dd5
Ergin, Huseyin <hergin@bsu.edu>
2019-02-05 22:12:46 -0500

    A new simplified interface for index page.

--- env.js
+++ env.js
@@ -13,58 +13,20 @@ $(document).ready(function(){
 
     var codeMirror = CodeMirror.fromTextArea(scxmlContent[0], window.location.search.match(/keyMap=vim/) ? {keyMap:'vim'} : undefined);
 
-    codeMirror.setValue('<scxml initial="start" version="1.0" xmlns="http://www.w3.org/2005/07/scxml"><!--   node-size-and-position x=0 y=0 w=910 h=820  -->\n'+
-'    <state id="start"><!--   node-size-and-position x=81.36 y=43 w=75 h=75  -->\n'+
-'     <onentry>\n'+
-'      <send delay="1s" event="tick"></send>\n'+
-'      <script>parent.all_off()</script>\n'+
-'     </onentry>\n'+
-'     <transition event="tick" target="lights"><!--   edge-path [lights]  pointx=0 pointy=0 offsetx=-84 offsety=0  --></transition>\n'+
-'    </state>\n'+
-'    <parallel id="new_node26"><!--   node-size-and-position x=260 y=40 w=380 h=700  -->\n'+
-'     <history id="H*" type="deep"><!--   node-size-and-position x=20 y=43 w=75 h=75  --></history>\n'+
-'     <state id="lights" initial="red"><!--   node-size-and-position x=169.61 y=120 w=160.5 h=513  -->\n'+
-'      <transition event="pause" target="turn_off"></transition>\n'+
-'      <state id="green"><!--   node-size-and-position x=20 y=293 w=75 h=75  -->\n'+
-'       <onentry>\n'+
-'        <send delay="5s" event="tick"></send>\n'+
-'        <script>parent.green()</script>\n'+
-'       </onentry>\n'+
-'       <transition event="tick" target="yellow"></transition>\n'+
-'      </state>\n'+
-'      <state id="yellow"><!--   node-size-and-position x=54 y=418 w=75 h=75  -->\n'+
-'       <onentry>\n'+
-'        <send delay="1s" event="tick"></send>\n'+
-'        <script>parent.yellow()</script>\n'+
-'       </onentry>\n'+
-'       <transition event="tick" target="red"><!--   edge-path [red]  x=125.5 y=330.5 x=125.5 y=205.5  --></transition>\n'+
-'      </state>\n'+
-'      <state id="greensoon"><!--   node-size-and-position x=20 y=168 w=75 h=75  -->\n'+
-'       <onentry>\n'+
-'        <send delay="1s" event="tick"></send>\n'+
-'        <script>parent.green_soon()</script>\n'+
-'       </onentry>\n'+
-'       <transition event="tick" target="green"></transition>\n'+
-'      </state>\n'+
-'      <state id="red"><!--   node-size-and-position x=54 y=43 w=75 h=75  -->\n'+
-'       <onentry>\n'+
-'        <send delay="5s" event="tick"></send>\n'+
-'        <script>parent.red()</script>\n'+
-'       </onentry>\n'+
-'       <transition event="tick" target="greensoon"></transition>\n'+
-'      </state>\n'+
-'     </state>\n'+
-'    </parallel>\n'+
-'    <state id="turn_off"><!--   node-size-and-position x=80.36 y=440 w=75 h=75  -->\n'+
-'     <onentry>\n'+
-'      <script>\n'+
-'   \n'+
-'   parent.all_off()\n'+
-'      </script>\n'+
-'     </onentry>\n'+
-'     <transition event="resume" target="H*"></transition>\n'+
-'    </state>\n'+
-'   </scxml>');
+    codeMirror.setValue('<?xml version="1.0" encoding="UTF-8"?>\n'+
+'    <scxml xmlns="http://www.w3.org/2005/07/scxml"\n'+
+'           version="1.0" initial="off">\n'+
+'        <state id="off">\n'+
+'            <transition event="turnOn" target="on">\n'+
+'                <script>parent.toggleBulb()</script>\n'+
+'            </transition>\n'+
+'        </state>\n'+
+'        <state id="on">\n'+
+'            <transition event="turnOff" target="off">\n'+
+'                <script>parent.toggleBulb()</script>\n'+
+'            </transition>\n'+
+'        </state>\n'+
+'    </scxml>');
 
     //add behaviour
     scxmlLoadInitControls.submit(function(e){
